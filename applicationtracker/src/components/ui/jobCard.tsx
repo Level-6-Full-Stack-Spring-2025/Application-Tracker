0414b031325d6aa454e7eb29fcfb70c0dd4664f3
Dishita <dishita@outlook.com>
2025-05-13 16:27:29 -0700

    mergin

--- applicationtracker/src/components/ui/jobCard.tsx
+++ applicationtracker/src/components/ui/jobCard.tsx
@@ -1,16 +1,44 @@
-interface Props {
-    label?: string;
-  }
-  
-  export default function JobCard({ label }: Props) {
-    return (
-      <div className="relative w-40 h-24 bg-gray-200 rounded">
-        {label && (
-          <span className="absolute top-1 left-1 bg-red-600 text-white text-xs px-1 rounded">
-            {label}
-          </span>
-        )}
-      </div>
+import { Badge } from "./badge";
+import { Button } from "./button";
+
+// TODO figure out how to get the badges as a prop (take in a)
+type Tag = {
+    title: string
+    variant: "default" | "progress" | "urgent" | null | undefined
+}
+
+type JobCardProps = {
+    jobTitle: string
+    location: string
+    tags: Tag[]
+}
+export default function JobCard({jobTitle, location, tags}:JobCardProps) {
+
+    return(
+        <div>
+            <div className="flex flex-col items-start bg-card w-90 h-50 rounded-lg px-7 py-5 justify-start text-white">
+                <h1 className="font-lalezar text-lg">{jobTitle}</h1>
+                <p className="font-inter text-xs">{location}</p>
+                <div className="pt-3 flex flex-row flex-wrap gap-1">
+                    {
+                        tags.sort((t1: Tag, t2: Tag)=>{
+                            if(t1.variant && !t2.variant) {
+                                return -1
+                            } else {
+                                return 1
+                            }
+                        }).map((tag)=> {
+                            return <Badge variant={tag.variant}>{tag.title}</Badge>
+                        })
+                    }
+                </div>
+                {/* TODO: look at what onclick redicrection looks like w/shadcn */}
+                <div className="flex items-end justify-end flex-row gap-3 font-lalezar w-77 h-50">
+                        <Button> Update </Button>
+                        <Button> Apply </Button>
+                </div>
+            </div>
+        </div>
     );
   }
   
